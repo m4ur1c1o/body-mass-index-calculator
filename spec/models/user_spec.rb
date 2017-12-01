6ac91325607520e7ab8eb81bd762c4c98967247c
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-11-30 20:25:12 -0600

    Test for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.create(:user) }
+
+  describe 'creation' do
+    it 'can be created' do
+      expect(@user).to be_valid
+    end
+  end
 end
