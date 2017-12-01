a610a54875ad5a91961f76baf951d775e3256122
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-11-30 20:01:56 -0600

    Added form for body measure

--- app/controllers/body_measures_controller.rb
+++ app/controllers/body_measures_controller.rb
@@ -1,2 +1,24 @@
 class BodyMeasuresController < ApplicationController
+  def new
+    @body_measure = BodyMeasure.new
+  end
+
+  def create
+    @body_measure = BodyMeasure.new(body_measure_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @body_measure.save
+        format.html { redirect_to new_body_measure_path, notice: 'Measure created' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def body_measure_params
+      params.require(:body_measure).permit(:height, :weight)
+    end
+
 end
--- app/models/body_measure.rb
+++ app/models/body_measure.rb
@@ -1,3 +1,4 @@
 class BodyMeasure < ApplicationRecord
   belongs_to :user
+  validates_presence_of :height, :weight
 end
